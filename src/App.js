430dff69ef465e2431ddd742507df90f88726cae
janaSunrise <warriordefenderz@gmail.com>
2021-04-04 13:22:23 +0530

    Add dynamicity in the features.

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
 
   return (
     <div className="container">
-      <Header onAdd={() => setShowAddTask(!showAddTask)} />
+      <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
       {showAddTask && <AddTask onAdd={addTask} />}
 
       {tasks.length > 0 ? (
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
 import PropTypes from "prop-types";
 import Button from "./Button";
 
-const Header = ({ title, onAdd }) => {
+const Header = ({ title, onAdd, showAdd }) => {
   return (
     <header className="header">
       <h1 style={{ fontWeight: "bold" }}>{title}</h1>
-      <Button color="green" text="Add" onClick={onAdd} />
+      <Button
+        color={showAdd ? "red" : "green"}
+        text={showAdd ? "Close" : "Add"}
+        onClick={onAdd}
+      />
     </header>
   );
 };
